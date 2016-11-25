605077e4d8a5484dbf8d530d331e9591ed816bcc
arhd83 <amir.haghighi83@yahoo.com>
2016-11-25 19:12:38 +0100

    comment integration test class

--- src/test/java/com/gft/bench/integration/TechnicalChallenge1IntegrationTest.java
+++ src/test/java/com/gft/bench/integration/TechnicalChallenge1IntegrationTest.java
@@ -1,34 +1,34 @@
-package com.gft.bench.integration;
+//package com.gft.bench.integration;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.context.embedded.LocalServerPort;
+//import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.web.socket.WebSocketHttpHeaders;
+//import org.springframework.web.socket.messaging.WebSocketStompClient;
+//import org.springframework.web.socket.sockjs.client.SockJsClient;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//public class TechnicalChallenge1IntegrationTest{
+//
+//    @LocalServerPort
+//    private int port;
+//
+//    private SockJsClient sockJsClient;
+//    private WebSocketStompClient webSocketStompClient;
+//
+//    private final WebSocketHttpHeaders webSocketHttpHeaders = new WebSocketHttpHeaders();
+//
+//    /**
+//     * we have a browser
+//     * she sent request to WebSocketBackEnd
+//     *
+//     */
+//    @Test
+//    public void shouldAllowTheClientToSubscribe(){
+//
+//    }
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.context.embedded.LocalServerPort;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.web.socket.WebSocketHttpHeaders;
-import org.springframework.web.socket.messaging.WebSocketStompClient;
-import org.springframework.web.socket.sockjs.client.SockJsClient;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class TechnicalChallenge1IntegrationTest{
-
-    @LocalServerPort
-    private int port;
-
-    private SockJsClient sockJsClient;
-    private WebSocketStompClient webSocketStompClient;
-
-    private final WebSocketHttpHeaders webSocketHttpHeaders = new WebSocketHttpHeaders();
-
-    /**
-     * we have a browser
-     * she sent request to WebSocketBackEnd
-     *
-     */
-    @Test
-    public void shouldAllowTheClientToSubscribe(){
-
-    }
-
-}
+//}
